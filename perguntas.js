c44d9f37ba1cd9bc439b31bcdc847ad011a65518
marianabeatrizp <148975050+marianabeatrizp@users.noreply.github.com>
2024-09-24 16:07:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'biologia ',
+    'O que é imposto?',
+    ' é todo tributo cuja obrigação tem por fato gerador uma situação independente de qualquer atividade estatal específica, relativa ao contribuinte'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da alemanha?',
+    'A capital da alemanha é Berlin'
+)
+
+criaCartao(
+    'historia',
+    'quanto tempo leva para um fato historico entrar pros livros?',
+    ' de 5 a 10 anos '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz mundo em ingles?',
+    'word'
+)
